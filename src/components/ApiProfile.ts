10108906bb80ea25b52d93b3652e9c8c7938472c
admin <krivalcevich@gmail.com>
2022-07-01 16:31:15 +0300

    add heroku

--- src/components/ApiProfile.ts
+++ src/components/ApiProfile.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 import {ParamsType} from "./api";
 
+// export const instance = axios.create({
+//     baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+//     withCredentials: true,
+// })
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    baseURL: process.env.REACT_APP_BACK_URL || ' https://neko-back.herokuapp.com/2.0',
     withCredentials: true,
 })
 
